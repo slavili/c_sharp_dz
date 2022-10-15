c16092928077892f344316fc275cfabf8baa65e7
Vyacheslav <vilisov.v.s@yandex.ru>
2022-10-15 23:21:10 +0700

    Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.

---
+++ Task008/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+int counter = 2; // Счётчик циклов, шаг будет равен 2, т.к. нужны чётные числа
+int userNumber; // число введённое пользователем
+string result = ""; // результирующая строка со всеми чётными числами разделёнными пробелом
+
+Console.Write("Введите число: ");
+userNumber = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Все чётные цифры от 1 до {userNumber}");
+
+while (counter <= userNumber)
+{
+    result += counter.ToString() + " ";
+    counter += 2;
+}
+
+Console.WriteLine(result);
