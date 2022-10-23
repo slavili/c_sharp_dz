54d4822afae3b66c70e78a74e2d36b740a2bfd32
Vyacheslav <vilisov.v.s@yandex.ru>
2022-10-23 19:42:08 +0700

    Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.

---
+++ Task023/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+void tableOfCubes(int userNumber, int degree = 3)
+{
+
+    for (int i = 1; i <= userNumber; i++)// счётчик с 1, т.к. возводить 0 в степень не имеет смысла
+    {
+        int result = 1;// каждый раз приводим result к нейтральному состоянию
+
+        for (int j = 0; j < degree; j++)
+        {
+            result *= i;
+        }
+        Console.Write(result + " ");
+    }
+
+}
+
+Console.Write("Введите число: ");
+int uNumber = int.Parse(Console.ReadLine()!);
+tableOfCubes(uNumber);
