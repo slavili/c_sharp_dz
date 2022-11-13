abc149943ff30c7b56e45789d79b7ce089f39afd
Vyacheslav <vilisov.v.s@yandex.ru>
2022-11-13 23:59:32 +0700

    Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.

---
+++ Task038/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+[3 7 22 2 78] -> 76
+*/
+//FillArray - функция наполнения массива
+int[] FillArray(int lengthArray = 10, int minNumber = 1, int maxNumber = 1000)
+{
+
+    int[] userArray = new int[lengthArray];
+    Random userRand = new Random();
+    for (int i = 0; i < lengthArray; i++)
+    {
+        userArray[i] = userRand.Next(minNumber, maxNumber + 1);
+    }
+    return userArray;
+}
+//DiffBetweenMaxMin - функция вычисления разности между max и min
+int DiffBetweenMaxMin(int[] userArray)
+{
+    int maxValue = userArray[0];
+    int minValue = userArray[0];
+
+    for (int i = 1; i < userArray.Length; i++)
+    {
+        if (userArray[i] > maxValue)
+        {
+            maxValue = userArray[i];
+        }
+        else if (userArray[i] < minValue)
+        {
+            minValue = userArray[i];
+        }
+    }
+    return maxValue - minValue;
+}
+
+int[] arrayOfDigits = FillArray();
+
+Console.WriteLine("Оригинальный массив: [" + string.Join(", ", arrayOfDigits) + "]");
+Console.WriteLine("Разность м/у Max и Min значениями = " + DiffBetweenMaxMin(arrayOfDigits));
