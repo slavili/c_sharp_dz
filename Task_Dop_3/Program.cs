977097c30ea198a19f9c73973ced6c6651c72dae
Vyacheslav <vilisov.v.s@yandex.ru>
2022-11-20 00:32:47 +0700

    Найдите MEX массива - Доработано

--- Task_Dop_3/Program.cs
+++ Task_Dop_3/Program.cs
@@ -2,19 +2,19 @@
 Дополнительно. Дан массив. Найдите MEX массива. MEX (Minimum EXcluded) - минимальный отсутствующий элемент.
 */
 //FillArray - функция заполнения массива
-int[] FillArray(int lengthArray = 10, int minNumber = 0, int maxNumber = 10)
+int[] FillArray(int lengthArray = 10, int maxNumber = 10)
 {
 
     int[] userArray = new int[lengthArray];
     Random userRand = new Random();
     for (int i = 0; i < lengthArray; i++)
     {
-        userArray[i] = userRand.Next(minNumber, maxNumber + 1);
+        userArray[i] = userRand.Next(maxNumber + 1);
     }
     return userArray;
 }
 //SearchMinDigit - функция поиска минимального недостающего числа (MEX)
-string SearchMinDigit(int[] userArray, bool isDebug = false)
+int SearchMinDigit(int[] userArray, bool isDebug = false)
 {
 
     System.Console.WriteLine("Оригинальный массив [" + string.Join(", ", userArray) + "]");
@@ -40,7 +40,7 @@ string SearchMinDigit(int[] userArray, bool isDebug = false)
             {
                 System.Console.WriteLine(userArray[j + 1] + 1);
             }
-            else return Convert.ToString(userArray[j + 1] + 1);
+            else return userArray[j + 1] + 1;
         }
     }
 
@@ -48,14 +48,17 @@ string SearchMinDigit(int[] userArray, bool isDebug = false)
     {
         System.Console.WriteLine("Отсортирован массив [" + string.Join(", ", userArray) + "]");
     }
-    return "Массив плотный!";
+
+    if (userArray.Last() - 1 >= 0)
+        return userArray.Last() - 1;
+    else return userArray[0] + 1;
 }
 
-int[] arrayOfDigits = FillArray(lengthArray: 20, minNumber: 0, maxNumber: 100);
+int[] arrayOfDigits = FillArray(lengthArray: 20, maxNumber: 10);
 //int[] arrayOfDigits = { 3, 8, 5, 7, 4, 3, 10, 9, 1, 10 };
 //int[] arrayOfDigits = { 2, 8, 5, 7, 4, 3, 10, 9, 1, 10 };
-//int[] arrayOfDigits = { -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };//плотный массив
-//int[] arrayOfDigits = { -5, 71, 18, 21, -4, 69, -38, -28, 77, 29, -9, -94, 74, -1, -85, 41, 30, -78, 44, 24 };
+//int[] arrayOfDigits = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };//плотный массив
+//int[] arrayOfDigits = { 1, 1, 1, 1, 1, 1, 1, 3, 2, 2, 2 };//плотный массив
 
 System.Console.WriteLine("MEX: " + SearchMinDigit(arrayOfDigits, isDebug: false));
 
