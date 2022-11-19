5b7b57fb3b4abd54bcef85b94fb5a8a786f0c11c
Vyacheslav <vilisov.v.s@yandex.ru>
2022-11-19 22:21:48 +0700

    Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.

---
+++ Task047/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+
+*/
+
+double[,] testArray = CreateArray();
+PrintArray(testArray);
+
+//CreateArray - функция наполнения массива случайными вещественными числами
+double[,] CreateArray(int rows = 3, int columns = 4, int orderOfMagnitude = 10, int roundUpTo = 1)
+{
+    double[,] array = new double[rows, columns];
+    double tempValue = 0;
+    var rnd = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            tempValue = Math.Pow(-1, rnd.Next(2)) * Math.Round(rnd.NextDouble() * orderOfMagnitude, rnd.Next(roundUpTo + 1));
+            //При генерации числа иногда генерируется минус ноль, с помощью if решил эту проблему.
+            if (Math.Sign(tempValue) == 0)
+            {
+                tempValue = 0;
+            }
+            array[i, j] = tempValue;
+        }
+    }
+    return array;
+}
+//PrintArray - функция вывода массива на монитор
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
